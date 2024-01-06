cf81e22c07eb48f1d44b4cd812f3bae09fc55c0c
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-01-06 13:10:36 +0530

    Epress generator setupo

---
+++ routes/post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestdb");
+const postSchema = new Schema({
+  postText: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestdb");
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  dp: {
+    type: String, // You can adjust the type based on how you store the profile picture.
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("User", userSchema);
