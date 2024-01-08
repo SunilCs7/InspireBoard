9bb8d477d05db1703d223fd634d1d100e6d7f87c
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-01-08 23:43:51 +0530

    Authentication(login, logout, /profile, register)-code and login page design

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,47 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./post");
+const passport = require("passport");
 
-/* GET home page. */
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
+
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Expresssunil" });
+  res.render("index", { title: "Express" });
+});
+router.get("/profile", isLogedIn, function (req, res, next) {
+  res.send("profile");
 });
 
+router.post("/register", function (req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+  userData.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "profile",
+    failureRedirect: "/",
+  }),
+  function () {}
+);
+
+router.post("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+function isLogedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
 module.exports = router;
